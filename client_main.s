9f5256b28570f3cace3fed6486558eefd9a76f5d
antheaml <65175117+antheaml@users.noreply.github.com>
2022-01-05 22:49:43 +0000

    Organising client code and commenting - imcomplete
    writing correct polling function, making sure the correct pins are set in the correct way

--- client_main.s
+++ client_main.s
@@ -1,30 +1,58 @@
 ;;; CLIENT CODE
 #include <xc.inc>
 
-extrn	SPI_MasterInit, SPI_MasterTransmit, SPI_MasterRead, UART_Setup, UART_Transmit_Byte;, read_byte1
-    
+extrn	SPI_MasterInit, SPI_MasterTransmit, SPI_MasterRead, UART_Setup, UART_Transmit_Byte, Client_Int_Hi
+
+; ---------- Module controlling the set-up and standby state of the client device ---------- ;
+
 psect	code, abs
 	
 rst:	org	0x0000	; reset vector
 	goto	clientSetup
 	
 int_hi:	org	0x0008	; High priority interrupt
-	goto	DAC_Int_Hi
-	
-Client_Int_Hi:	
+	goto	Client_Int_Hi
+
+clientSetup:	
+	call	Client_pin_Setup ; enable pins as inputs and outputs, turn on standby led
 	call	Client_Interrupt_Setup ; Set up interrupt
 	call	Client_Led_Setup ; Set up LEDs NEED TO WRITE
 	call	client_imuSetup
+	goto	client_main_polling
+	
+client_main_polling: 
+    ; *************************************************************************************************
+    ; ----- Looping subroutine which tests the logic level of RD1:2 to determine if the alert or
+    ; ----- disable buttons are pressed on the client device
+    ; ----- Logic:
+    ; -----		Fall occurs -> PORTB0 high -> interrupt triggered -> go to Client_Int_Hi 
+    ; -----		Alert button pressed -> PORTD1 high -> call alert subroutine 
+    ; ----- 		Disable button pressed -> PORTD2 high -> call disable subroutine triggered
+    ; ----- BTFSC = bit test f, skip if clear
+    ; *************************************************************************************************
+    
+    BTFSC   PORTD, 1, A 
+    call    client_alert
+    BTFSC   PORTD, 0, A 
+    call    nurse_fall
+    BTFSC   PORTD, 2, A 
+    call    nurse_remote_disable
+    bra     polling_main ; loop
 	goto	$	; Sit in infinite loop
 
-main:
-    goto    $
 
-Client_Led_Setup:
-    bcf	    PORTD, 4, A ; enable RD4 (fall LED) as output
-    bcf	    PORTE, 4, A ; enable RE4 (alert button) as output
-    
-
+Client_pin_Setup:
+	; Enable RH0:2 pins as outputs
+	bcf	TRISH, 0, A
+	bcf	TRISH, 1, A
+	bcf	TRISH, 2, A
+	; Turn on disable LED
+	bsf	PORTH, 2, A
+	; Enable RD1:2 as inputs
+	bsf	TRISD, 1, A
+	bsf	TRISD, 2, A
+	return
+  
 test_read:
     ;################# CHIP_ID address 0x00 but MSB = 1 (Read)
     call    NOP_delay
