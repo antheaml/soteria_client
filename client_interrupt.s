eba0697dacebab832bd09bd3e01ed899d974898b
antheaml <65175117+antheaml@users.noreply.github.com>
2022-01-05 22:49:59 +0000

    Organising code

--- client_interrupt.s
+++ client_interrupt.s
@@ -15,34 +15,23 @@ Client_Interrupt_Setup: ; used to be DAC_Setup
 	; need to rewrite and set up based on how the client is wired up
 	movlw	0xFF
 	movwf	TRISB, A ; enable RB pins as inputs
-	movlw	0x00
-	movwf	TRISH, A ; enable H pins as outputs
-	bsf	RBIE		; Enable RB interrupt				    Not sure if these codes are compatable with the clicker, need to check, not in manual online?
+	bsf	RBIE		; Enable RB interrupt				   
 	bsf	GIE		; Enable all interrupts
 	return
 
 Client_Int_Hi:	; used to be DAC_Int_Hi
-	btfss	RBIF		; check that this is RB interrupt		    Is this the same for the clicker?
+	btfss	RBIF		; check that this is RB interrupt		 
 	retfie	f		; if not then return
 	; logic to figure out which subroutine to call depending on signal
-	BTFSC	PORTB, 0, A  ;bit test RB0, skip if clear - alert button
-	goto    client_alert
-	BTFSC	PORTB, 1, A  ;bit test RB1, skip if clear - disable button
-	goto	client_disable
 	call	client_fall
-end_Client_Int_Hi:
 	bcf	RBIF		; clear interrupt flag
 	retfie	f		; fast return from interrupt	
 	
 client_fall:
-    bsf	    PORTH, 4, A ; make RH4 high (stays high until disable)		   
-    bsf	    PORTD, 4, A ; turn on fall LED
+    bsf	    PORTH, 0, A ; make RH4 high (stays high until disable)		   
+    bsf	    PORTD, 0, A ; turn on fall LED
     return
-	; write interrupt subroutine that is called when fall 
-	    ; RB0 which triggers
-	    ; make RH0 high (stays high)
-	    ; RH4 connects RB0 on nurse
-	        
+
 client_alert:	 
     bsf	    PORTH, 1, A ; make RH7 high (stays high)				   
     bsf	    PORTE, 4, A ; turn on alert button LED
