4f92675b952d91826d95231d3e8badaf5d057be7
antheaml <65175117+antheaml@users.noreply.github.com>
2022-01-07 00:16:08 +0000

    Updated client_interrupt.s
    Added comments and made sure the correct pins are being turned on and off. Separate output pins for LEDs and signal. A delay after signal turned on to allow the nurse to pick up signal, since the nurse is using polling.

--- client_interrupt.s
+++ client_interrupt.s
@@ -1,55 +1,78 @@
 #include <xc.inc>
 	
-global DAC_Int_Hi, Interrupt_Setup
-    
-extrn	nurse_fall, nurse_alert, nurse_remote_disable
-        
-psect udata_acs
-delay_counter1:	    ds 1
-delay_counter2:	    ds 1
-alert_code:	    ds 1
+global Client_Int_Hi, Client_Interrupt_Setup, client_fall, client_alert, client_disable
 
-psect	dac_code, class=CODE
+; ---------- Module controlling the fall interrupt and button responses of the client device ---------- ;
+
+psect	int_code, class=CODE
     
-Client_Interrupt_Setup: ; used to be DAC_Setup
-	; need to rewrite and set up based on how the client is wired up
+Client_Interrupt_Setup: 
+	; ****************************************************
+	; ----- Subroutine to set up RB interrupts for the IMU
+	; ****************************************************
 	movlw	0xFF
-	movwf	TRISB, A ; enable RB pins as inputs
+	movwf	TRISB, A 	; enable RB pins as inputs
 	bsf	RBIE		; Enable RB interrupt				   
 	bsf	GIE		; Enable all interrupts
 	return
 
-Client_Int_Hi:	; used to be DAC_Int_Hi
-	btfss	RBIF		; check that this is RB interrupt		 
+Client_Int_Hi:
+	; ******************************************************
+	; ----- Subroutine called when RB interrupt is triggered
+	; ******************************************************
+	BTFSS	RBIF		; check that this is RB interrupt		 
 	retfie	f		; if not then return
-	; logic to figure out which subroutine to call depending on signal
 	call	client_fall
 	bcf	RBIF		; clear interrupt flag
 	retfie	f		; fast return from interrupt	
 	
 client_fall:
-    bsf	    PORTH, 0, A ; make RH4 high (stays high until disable)		   
-    bsf	    PORTD, 0, A ; turn on fall LED
-    return
+	; *****************************************
+	; ----- Subroutine called when client falls
+	; *****************************************
+        bcf     PORTH, 2, A ; turn off disable signal
+        bcf     PORTH, 5, A ; turn off disable LED
+        bcf	PORTH, 1, A ; turn off alert signal 
+        bcf     PORTH, 4, A ; turn off alert LED
+        bsf     PORTH, 0, A ; turn on fall signal
+        bsf	PORTH, 0, A ; turn on fall LED
+	NOP		    ; NOPs required so there is enough time for the nurse to pick up the signal
+        NOP	
+        NOP
+        NOP
+        bcf     PORTH, 0, A ; turn off fall signal to nurse
+        return
 
-client_alert:	 
-    bsf	    PORTH, 1, A ; make RH7 high (stays high)				   
-    bsf	    PORTE, 4, A ; turn on alert button LED
-    goto    end_Client_Int_Hi
-	; write interrupt subroutine this is called when alert button pressed
-	    ; RB1 which triggers
-	    ; make RH1 high (stays high)
-	    ; RH1 connects RB1 on nurse
+client_alert:
+	; ***********************************************************
+	; ----- Subroutine called when client alert button is pressed
+	; ***********************************************************
+	bcf	PORTH, 2, A ; turn off disable signal
+	bcf	PORTH, 5, A ; turn off disable LED
+	bcf	PORTH, 0, A ; turn off fall signal
+	bcf	PORTH, 3, A ; turn off fall LED
+	bsf	PORTH, 1, A ; turn on alert signal
+	bsf	PORTH, 4, A ; turn on alert LED
+	NOP		    ; NOPs required so there is enough time for the nurse to pick up the signal
+	NOP	
+	NOP
+	NOP
+	bcf     PORTH, 1, A ; turn off alert signal to nurse			   
+	return
+
 client_disable:	
-    bcf	    PORTH, 4, A ; make RH4 low						  
-    bcf	    PORTH, 7, A ; make RH7 low						    
-    bcf	    PORTD, 4, A ; turn fall LED off
-    bcf	    PORTE, 4, A ; turn alert LED off
-    goto    end_Client_Int_Hi
-	; write interrupt subroutine this is called when disable button pressed
-	    ; RB2 which triggers
-	    ; make RH2 high (stays high)
-	    ; RH2 connects RB2 on nurse
-
-
-
+	; *************************************************************
+	; ----- Subroutine called when client disable button is pressed
+	; *************************************************************
+    	bcf	PORTH, 1, A ; turn off alert signal
+    	bcf	PORTH, 4, A ; turn off alert LED
+   	bcf	PORTH, 0, A ; turn off fall signal
+    	bcf	PORTH, 3, A ; turn off fall LED
+    	bsf	PORTH, 2, A ; turn on disable signal
+    	bsf	PORTH, 5, A ; turn on disable LED
+    	NOP		    ; NOPs required so there is enough time for the nurse to pick up the signal
+    	NOP	
+    	NOP
+    	NOP
+    	bcf     PORTH, 2, A ; turn off disable signal to nurse					    
+    	return
